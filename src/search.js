f87475b6e4a3b7bbfdca4f485072d900254212c7
Rivky Bleier <rivky@ezrirx.com>
2020-05-19 22:11:43 -0400

    added styles and components

---
+++ src/search.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import MovieCard from "./MovieCard";
+
+export default function Search() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=37ccdb3b0abc6c9b3c6cb9e658b3ef39&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          type="text"
+          className="input"
+          name="query"
+          placeholder="Search for a movie..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (movie = <MovieCard movie={movie} key={movie.id} />))}
+      </div>
+    </>
+  );
+}
