247ca331e85460257b99f739c61e42108c9c853d
RivkyBleier <59883988+RivkyBleier@users.noreply.github.com>
2020-09-13 12:34:38 -0400

    Updates

---
+++ precache-manifest.2ac774455723cebef986e382c34a53ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f13f16fa2d1771ffba9e5f6cf2d0b7",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "2ac2b01dc06b64a09bd9",
+    "url": "/movie-search/static/css/main.ee71f731.chunk.css"
+  },
+  {
+    "revision": "09b7e4a8caf93bafeadc",
+    "url": "/movie-search/static/js/2.914ebb8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.914ebb8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac2b01dc06b64a09bd9",
+    "url": "/movie-search/static/js/main.bb1f54fa.chunk.js"
+  },
+  {
+    "revision": "08d1cf6968dfcbb7f688",
+    "url": "/movie-search/static/js/runtime-main.fd4c7407.js"
+  }
+]);
